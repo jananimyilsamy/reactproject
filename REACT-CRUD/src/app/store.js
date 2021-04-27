bcb3bfe101dcd2b85e8168a99e5db2ad5212fc4e
jananimyilsamy <49332397+jananimyilsamy@users.noreply.github.com>
2021-04-27 16:19:06 +0530

    Delete store.js

--- REACT-CRUD/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {useReducer} from 'react';
-//import userRedux from '../features/userSlice';
-
-export default configureStore ({
-  reducer: {
-    user: useReducer,
-  },
-});
